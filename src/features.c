5ec6b9f14844b572f805ecc8890c4208db3e1291
JustinP <justin.pratx@etu.estia.fr>
2025-05-21 16:11:25 +0200

    dimension c

--- src/features.c
+++ src/features.c
@@ -11,6 +11,19 @@
  * When the feature is totally implemented, your commit message must contain "close #n".
  */
 
-void helloWorld() {
-    printf("Hello World !");
+void dimension(char *source_path) {
+    unsigned char *data = NULL;
+    int width = 0, height = 0, channels = 0;
+
+    // read_image_data renvoie 0 en cas de succès (défini dans estia-image.h)
+    if (read_image_data(source_path, &data, &width, &height, &channels) != 0) {
+        fprintf(stderr, "Erreur : impossible de lire l'image \"%s\"\n", source_path);
+        return;
+    }
+
+    // Affichage du résultat
+    printf("dimension: %d, %d\n", width, height);
+
+    // Libération de la mémoire allouée par read_image_data
+    free(data);
 }
